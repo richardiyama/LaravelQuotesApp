ae30361e4eea0a3e44a1908c5e10fa644beb0a98
Richard Iyama <richardiyama@yahoo.com>
2017-02-26 18:53:15 +0100

    first commit

---
+++ resources/views/email/user_notification.blade.php
@@ -0,0 +1,2 @@
+<h1>Thanking you for creating a Quote {{ $name }} </h1>
+<p>Please register here <a href="{{route('mail_callback',['author_name' => $name])}}">Link</a></p>
---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['middleware' =>['web']], function(){
+Route::get('/{author?}', [
+'uses' => 'QuoteController@getIndex',
+'as' => 'index'
+
+	]);
+
+Route::post('/new',[
+'uses' => 'QuoteController@postQuote',
+'as' =>'create'
+	]);
+
+Route::get('/delete/{quote_id}', [
+'uses' => 'QuoteController@getDeleteQuote',
+'as'   => 'delete'
+	]);
+
+Route::get('/gotemail/{author_name}', [
+ 
+ 'uses' => 'QuoteController@getMailCallback',
+ 'as'   => 'mail_callback'
+
+	]);
+Route::get('/admin/login',[
+'uses'  => 'AdminController@getLogin',
+'as'    =>  'admin.login'
+
+	]);
+
+Route::post('/admin/login',[
+'uses'  => 'AdminController@postLogin',
+'as'    =>  'admin.login'
+
+	]);
+
+Route::get('/admin/logout', [
+	'uses' => 'AdminController@getLogout',
+	'as' => 'admin.logout'
+]);
+
+
+Route::group(['middleware' => 'auth'], function(){
+Route::get('/admin/dashboard', [
+	'uses' => 'AdminController@getDashboard',
+	'as' => 'admin.dashboard'
+]);
+
+Route::get('/admin/quotes', function(){
+	return view('admin.quotes');
+});
+
+});
+
+});
