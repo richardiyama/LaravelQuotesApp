22d1ae749872b15f4d32ed9ca66f994b8a320045
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-09 19:46:34 +0100

    Update QuoteCreated.php

--- app/Events/QuoteCreated.php
+++ app/Events/QuoteCreated.php
@@ -3,6 +3,7 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
+
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
