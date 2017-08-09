34fb7dfc44d106b3a99952758db039707953cf68
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-09 19:47:03 +0100

    Update QuoteCreated.php

--- app/Events/QuoteCreated.php
+++ app/Events/QuoteCreated.php
@@ -5,6 +5,7 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 
 use Illuminate\Queue\SerializesModels;
+
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
