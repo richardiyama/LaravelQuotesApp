70c9fa6e402c2bdbb17c2e69fc8c0adab8ecadd0
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-09 19:58:11 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Support\Facades\Validator;
+
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
